52987df2cf3c82c6e249f0d58dd41d4a16d84263
MiaAltieri <mgaltier200@gmail.com>
2024-08-19 09:11:45 +0000

    WIP on dataplatform helpers

--- data_platform_helpers/tests_helpers/vms/ha_helpers.py
+++ data_platform_helpers/tests_helpers/vms/ha_helpers.py
@@ -3,6 +3,7 @@
 
 import subprocess
 from pytest_operator.plugin import OpsTest
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 
 def cut_network_from_unit_with_ip_change(machine_name: str) -> None:
@@ -50,7 +51,67 @@ async def restore_network_for_unit_without_ip_change(machine_name: str) -> None:
     subprocess.check_call(limit_set_command.split())
 
 
-# TODO add these network helpers:
+def is_machine_reachable_from(origin_machine: str, target_machine: str) -> bool:
+    """Test network reachability between hosts.
+
+    Args:
+        origin_machine: hostname of the machine to test connection from
+        target_machine: hostname of the machine to test connection to
+    """
+    try:
+        subprocess.check_call(f"lxc exec {origin_machine} -- ping -c 5 {target_machine}".split())
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+@retry(stop=stop_after_attempt(60), wait=wait_fixed(15))
+def assert_ip_different_after_retore(model_name: str, hostname: str, old_ip: str) -> bool:
+    """Wait until network is restored.
+
+    Args:
+        model_name: The name of the model
+        hostname: The name of the instance
+        old_ip: old registered IP address
+    """
+    assert get_unit_ip(model_name, hostname) == old_ip, "IP address has not changed yet."
+
+
+@retry(stop=stop_after_attempt(20), wait=wait_fixed(15))
+async def wait_network_restore(ops_test: OpsTest, unit_name: str) -> None:
+    """Wait until network is restored.
+
+    Args:
+        ops_test: The ops test object passed into every test case
+        unit_name: The name of the unit
+        old_ip: old registered IP address
+    """
+    return_code, stdout, _ = await ops_test.juju("ssh", unit_name, "ip", "a")
+    if return_code != 0:
+        raise Exception
+
+    juju_unit_ip = await get_unit_ip(ops_test, unit_name)
+
+    if juju_unit_ip in stdout:
+        raise Exception
+
+
+async def get_unit_ip(ops_test: OpsTest, unit_name: str) -> str:
+    """Wrapper for getting unit ip.
+
+    Args:
+        ops_test: The ops test object passed into every test case
+        unit_name: The name of the unit to get the address
+    Returns:
+        The (str) ip of the unit
+    """
+    app_name = unit_name.split("/")[0]
+    unit_num = unit_name.split("/")[1]
+    status = await ops_test.model.get_status()  # noqa: F821
+    address = status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["public-address"]
+    return address
+
+
+# TODO add these remaining network helpers:
 # get_controller_machine
-# is_machine_reachable_from
 # wait_network_restore
