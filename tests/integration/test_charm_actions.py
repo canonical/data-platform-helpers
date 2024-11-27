bd33bd8d3691fdea2f5b8bb2e8fbf0713d17955f
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2024-11-27 15:30:29 +0000

    Add integration tests

---
+++ tests/integration/test_charm_actions.py
@@ -0,0 +1,63 @@
+import logging
+
+from data_platform_helpers.charm_actions import get_password, set_password
+from pytest_operator.plugin import OpsTest
+import pytest
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def test_charm():
+    return {
+        "deploy_args": {
+            "entity_url": "postgresql-k8s",
+            "channel": "14/stable",
+            "series": "jammy",
+            "revision": 281,
+            "num_units": 1,
+            "application_name": "postgresql",
+            "trust": True,
+        },
+        "test_username": "operator",
+    }
+
+
+async def test_deploy_charm(ops_test: OpsTest, test_charm):
+    """Deploy the postgresql-k8s charm and see it deploys successfully and settles to active state."""
+    logger.info("Deploying test charm...")
+    deploy_args = test_charm["deploy_args"]
+    (await ops_test.model.deploy(**deploy_args),)
+
+    logger.info("Waiting for test charm to be idle and active...")
+    await ops_test.model.wait_for_idle(
+        apps=[deploy_args["application_name"]], timeout=1000, status="active"
+    )
+
+
+async def test_get_password(ops_test: OpsTest, test_charm):
+    password = await get_password(
+        ops_test=ops_test,
+        app_name=test_charm["deploy_args"]["application_name"],
+        username=test_charm["test_username"],
+    )
+    assert password is not None
+    assert len(password) > 0
+
+
+async def test_set_password(ops_test: OpsTest, test_charm):
+    new_password = "foobar"
+    result = await set_password(
+        ops_test=ops_test,
+        app_name=test_charm["deploy_args"]["application_name"],
+        username=test_charm["test_username"],
+        password=new_password,
+    )
+    assert new_password == result
+
+    get_password_result = await get_password(
+        ops_test=ops_test,
+        app_name=test_charm["deploy_args"]["application_name"],
+        username=test_charm["test_username"],
+    )
+    assert get_password_result == new_password
